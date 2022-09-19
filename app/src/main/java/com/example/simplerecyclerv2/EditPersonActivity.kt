ffed9d0905dcf2e8cb00c8d2f879f8b6178834eb
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-09-19 18:14:14 +0300

    Add some fotos

--- app/src/main/java/com/example/simplerecyclerv2/EditPersonActivity.kt
+++ app/src/main/java/com/example/simplerecyclerv2/EditPersonActivity.kt
@@ -11,7 +11,9 @@ class EditPersonActivity : AppCompatActivity() {
     private var imageIdRes = R.drawable.man_01
     private val listImages = listOf(
         R.drawable.man_01, R.drawable.man_02, R.drawable.man_03, R.drawable.man_04,
-        R.drawable.girl_01, R.drawable.girl_02, R.drawable.girl_03, R.drawable.girl_04
+        R.drawable.man_05, R.drawable.man_06, R.drawable.man_07,
+        R.drawable.girl_01, R.drawable.girl_02, R.drawable.girl_03, R.drawable.girl_04,
+        R.drawable.girl_05, R.drawable.girl_06, R.drawable.girl_07, R.drawable.girl_08
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
